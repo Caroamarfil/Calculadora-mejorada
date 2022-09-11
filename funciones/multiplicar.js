184c9fd426ea36d5cf19e1b1bcba342a18757281
Caroamarfil <caroamarfil@outlook.com>
2022-09-11 00:06:22 -0300

    Calculadora mejorada con argv

--- funciones/multiplicar.js
+++ funciones/multiplicar.js
@@ -1,5 +1,9 @@
-const multiplicar = (numero1, numero2) => {
-   const resultado =numero1 * numero2;
+const multiplicar = (num1, num2) => {
+   if(num1 === 0 || num2 ===  0) {
+       return "No se puede multiplicar por 0. Ingrese otro número.";
+
+   }
+   const resultado =num1 * num2;
    return resultado;
 
 
